1249a48f81acddd52effe59285a63c8a783c37d0
Henry Seigbein <hensco95@gmail.com>
2024-01-03 12:03:08 +0100

    added changes to html, added dark and light mode functionality

---
+++ app.js
@@ -0,0 +1,45 @@
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+
+// display light or dark mode button dynamically 
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  // show light icon by removing hidden class
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+// toogle button
+
+themeToggleBtn.addEventListener("click", toggleMode);
+
+function toggleMode() {
+  // toggle Icon
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  } else {
+    // if theme isn't in local storage
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+}
